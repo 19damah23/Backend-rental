a8e76888b910f1e16888844b42145b76e6e2c1b1
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-08-16 14:21:05 +0700

    make connection to db

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(fileUpload());
+// app.use('v1', router);
+
+app.use('*', (req, res) => {
+  res.status(404).json({
+    message: 'url not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(req.status || 500).json({
+    message: err.message
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on ${process.env.PORT}`);
+});
